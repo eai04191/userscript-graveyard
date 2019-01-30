cc556547cc3d3717aad2be2a40511fc4c00c377d
Eai <eai@mizle.net>
2019-01-31 03:29:06 +0900

    Create notestock.loginCodeButton.user.js

---
+++ notestock.loginCodeButton.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         notestock - Add Login Code Toot Button.
+// @namespace    https://mizle.net/
+// @version      0.1
+// @description  Add Login Code Toot Button in notestock authentication screen.
+// @author       Eai <eai@mizle.net>
+// @match        https://notestock.osa-p.net/reserve.html
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const login_code = document.getElementById("code").value;
+
+    const accts = document.querySelector(".card-body").innerText.match(/@.+?@.+? /g);
+    const login_instance = accts[0].trim().match(/[^@]*$/);
+
+    const button = document.getElementById("btn_next");
+    button.insertAdjacentHTML('beforeBegin', `<a href="https://${login_instance}/share?text=${login_code}" class="btn btn-secondary" target="_blank">投稿する</a>`);
+})();
