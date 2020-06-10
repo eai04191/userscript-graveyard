202a15fd6cc3a01334e37f8ec81a2234aad3604e
Irie Aoi <eai@mizle.net>
2020-06-10 13:20:46 +0900

    敗北

--- userscript/twitter.betterArrow.user.js
+++ userscript/twitter.betterArrow.user.js
@@ -2,7 +2,7 @@
 // @name         Twitter Better Arrow
 // @author       Eai <eai@mizle.net>
 // @license      MIT
-// @version      0.0.1
+// @version      1.0.0
 // @match        https://twitter.com/*
 // @require      https://cdn.jsdelivr.net/npm/@violentmonkey/shortcut@1
 // ==/UserScript==
@@ -35,11 +35,11 @@
         return getTweetInstance().memoizedProps.children.props.children.props;
     };
 
-    VM.registerShortcut("ArrowLeft", () => {
+    VM.registerShortcut("ArrowUp", () => {
         const currentPage = parseInt(document.URL.split("/").reverse()[0]);
         const tweetElement = getTweetElement();
 
-        // 今のページが1の時にArrowLeftが押されたら前のツイートに行きたい
+        // 今のページが1の時にArrowUpが押されたら前のツイートに行きたい
         if (currentPage === 1) {
             console.log("前のツイートに行くべき");
 
@@ -47,16 +47,20 @@
                 .querySelector(`a[href*="retweets"]`) // retweetsだったりretweets/with_commentsだったりするので*
                 .closest(`div:not([class])`).previousSibling;
             beforeTweet.querySelector(`a[href$="/photo/4"]`).click();
+        } else {
+            document
+                .querySelectorAll(`[role="button"] [style*="9999px"]`)[0]
+                .click();
         }
     });
 
-    VM.registerShortcut("ArrowRight", () => {
+    VM.registerShortcut("ArrowDown", () => {
         const currentPage = parseInt(document.URL.split("/").reverse()[0]);
         const tweetElement = getTweetElement();
         const tweet = getTweetObj();
         const mediaCount = tweet.focalTweet.extended_entities.media.length;
 
-        // 今のページがメディアの添付数と同じ時にArrowRightが押されたら次のツイートに行きたい
+        // 今のページがメディアの添付数と同じ時にArrowDownが押されたら次のツイートに行きたい
         if (currentPage === mediaCount) {
             console.log("次のツイートに行くべき");
 
@@ -64,6 +68,15 @@
                 .querySelector(`a[href*="retweets"]`)
                 .closest(`div:not([class])`).nextSibling.nextSibling;
             nextTweet.querySelector(`a[href$="/photo/1"]`).click();
+        } else {
+            const arrows = document.querySelectorAll(
+                `[role="button"] [style*="9999px"]`
+            );
+            if (arrows[1]) {
+                arrows[1].click();
+            } else {
+                arrows[0].click();
+            }
         }
     });
 })();
