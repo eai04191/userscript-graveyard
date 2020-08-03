401ea5e8dfc56dd51437c94032c7f15eed0b7ee3
Irie Aoi <eai@mizle.net>
2020-08-04 05:05:36 +0900

    コメント数を追加

--- userscript/steamcommunity.rimWorld.moddb.info.user.js
+++ userscript/steamcommunity.rimWorld.moddb.info.user.js
@@ -2,8 +2,9 @@
 // @name         Steamワークショップ Rimworld Modデータベース統合
 // @author       Eai <eai@mizle.net>
 // @license      MIT
-// @version      1.0.0
+// @version      1.0.1
 // @match        https://steamcommunity.com/sharedfiles/filedetails/
+// @match        https://steamcommunity.com/workshop/filedetails/
 // @require      https://raw.githubusercontent.com/mitchellmebane/GM_fetch/master/GM_fetch.js
 // @grant        GM_xmlhttpRequest
 // ==/UserScript==
@@ -20,14 +21,15 @@
         return q("#PublishedFileSubscribe [name='appid']").value === "294100";
     };
 
-    const addDescripton = (isDescriptionAvilable, isJaTranslationAvailable) => {
+    const addDescripton = ({ isDescriptionAvilable, isJaTranslationAvailable, commentCount }) => {
         const divider = `<div class="dotted_hr_uri hr padded"></div>`;
         const link = `<a href="${dbURL}" target="_blank">RimWorld Mod データベース</a>`;
         const text = isDescriptionAvilable
             ? "にこのMODの説明があります。"
             : "にこのMODの説明はありませんでした。";
-        const text2 = isJaTranslationAvailable ? "また、日本語化ファイルがあります。" : "";
-        const html = `${divider}<p>${link}${text}<br>${text2}</p>`;
+        const text2 = commentCount ? `コメントが${commentCount}件あります。` : "";
+        const text3 = isJaTranslationAvailable ? "また、日本語化ファイルがあります。" : "";
+        const html = `${divider}<p>${link}${text}<br>${text2}<br>${text3}</p>`;
         q(".workshop_item_header .responsive_local_menu").insertAdjacentHTML("beforeend", html);
     };
 
@@ -40,6 +42,12 @@
         return !!dom.querySelector(".jpUpFilesBox");
     };
 
+    const getCommentCount = dom => {
+        return dom.querySelector(".comNum")
+            ? dom.querySelector(".comNum").textContent.replace("#", "")
+            : null;
+    };
+
     if (!isRimWorldWorkshop()) {
         return;
     }
@@ -47,5 +55,9 @@
     const response = await GM_fetch(dbURL);
     const html = await response.text();
     const doc = new DOMParser().parseFromString(html, "text/html");
-    addDescripton(isDescriptionAvilable(doc), isJaTranslationAvailable(doc));
+    addDescripton({
+        isDescriptionAvilable: isDescriptionAvilable(doc),
+        isJaTranslationAvailable: isJaTranslationAvailable(doc),
+        commentCount: getCommentCount(doc),
+    });
 })(document.querySelector.bind(document));
