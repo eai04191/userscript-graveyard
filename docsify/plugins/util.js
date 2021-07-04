46f749c099f462620f41dc0676e03346f05f2841
Irie Aoi <eai@mizle.net>
2021-07-05 00:08:52 +0900

    index.htmlじゃないときに動くように修正

--- docsify/plugins/util.js
+++ docsify/plugins/util.js
@@ -1,12 +1,13 @@
 /** ページパスから拡張子などを返す */
 export const getInfo = (filename) => {
+    console.log(filename);
     if (filename.includes("README.md")) return false;
     switch (true) {
-        case /^\/adblockfilter\//.test(filename):
+        case /^adblockfilter\//.test(filename):
             return { type: "adblockfilter", title: "Open this Filter", ext: ".txt" };
-        case /^\/userscript\//.test(filename):
+        case /^userscript\//.test(filename):
             return { type: "userscript", title: "Install this Script", ext: ".user.js" };
-        case /^\/userstyle\//.test(filename):
+        case /^userstyle\//.test(filename):
             return { type: "userstyle", title: "Install this Style", ext: ".user.styl" };
         default:
             return false;
